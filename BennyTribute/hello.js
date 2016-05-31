0b3858cf1d6ad0bc81010db170de9b8acc5289e1
drcircuit <espen@sandelarsen.com>
2016-05-31 22:59:37 +0200

    refactored code to avoid 'new' 'this' and 'for' as they are considered harmful.

--- BennyTribute/hello.js
+++ BennyTribute/hello.js
@@ -1,64 +1,88 @@
 /**
  * Original code written by @seb_ly, as presented on Trondheim Developer Conference in 2015
- * No animals were harmed during the production of this code 
- * 
+ * No animals were harmed during the production of this code
+ *
  * @digitaldias
+ *
+ * refactored code to avoid using "for", "this" and "new" as they are considered harmful in JavaScript.
+ * This code passes JSLint.com if you assume browser.
  */
-var canvas    = document.createElement("canvas");
-canvas.width  = window.innerWidth;
-canvas.height = window.innerHeight;
-document.body.appendChild(canvas);
 
-var ctx  = canvas.getContext("2d");
-var tree = new Branch(100);
-animate();
+var treeHeight = 120;
 
-
-function animate()
-{
-    ctx.save();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.translate(canvas.width/2, canvas.height - 100);
-    
-    tree.render();
-    
-    window.requestAnimationFrame(animate);
-    ctx.restore();
-}
-
-
-function Branch(size, rotation)
-{
-    this.children = [];
-    this.sway  = 0;
-    this.swaySpeed = random(0.02, 0.2);
-    
-    if(size>15){
-        this.children.push(new Branch(size * random(0.7, 0.9), random(15, 30)));
-        this.children.push(new Branch(size * random(0.7, 0.9), random(-15, -30)));
+((function (treeHeight) {
+    'use strict';
+    function random(max, min) {
+        return Math.random() * (max - min) + min;
     }
-    
-    this.render = function()
-    {
-        ctx.save();        
-        ctx.rotate((rotation  + Math.sin(this.sway)) * Math.PI/180);
-        ctx.beginPath();
-        ctx.lineWidth   = size * 0.1;
-        ctx.strokeStyle = 'White';
-        ctx.moveTo(0, 0);
-        ctx.lineTo(0, -size);
-        ctx.stroke();
-        ctx.translate(0, -size);
-        
-        for(var i = 0; i < this.children.length; i++){
-            this.children[i].render();
+
+    function setupCanvas() {
+        var newCanvas = document.createElement("canvas");
+        newCanvas.width = window.innerWidth;
+        newCanvas.height = window.innerHeight;
+        document.body.appendChild(newCanvas);
+        document.body.style.backgroundColor = 'black';
+        return newCanvas;
+    }
+    var canvas = setupCanvas();
+    var ctx = canvas.getContext("2d");
+
+
+    function branch(size, rotation, first) {
+        var children = [];
+        var sway = 0;
+        var swaySpeed = random(0.02, 0.2);
+
+        if (size > 15) {
+            children.push(branch(size * random(0.7, 0.9), random(15, 30), false));
+            children.push(branch(size * random(0.7, 0.9), random(-15, -30), false));
         }
-        this.sway += this.swaySpeed;
-        ctx.restore();                
-    }    
-}
+
+        var incrementSway = function () {
+            sway += swaySpeed;
+        };
+
+        var getSway = function () {
+            return sway;
+        };
+
+        var render = function (context) {
+            context.save();
+            context.rotate((rotation + Math.sin(getSway())) * Math.PI / 180);
+            context.beginPath();
+            context.lineWidth = size * 0.1;
+            context.strokeStyle = 'White';
+            context.moveTo(0, 0);
+            context.lineTo(0, -size);
+            context.stroke();
+            context.translate(0, -size);
+            children.forEach(function (child) {
+                child.render(context);
+            });
+            incrementSway();
+            context.restore();
+        };
 
 
-function random(max, min){
-    return Math.random() * (max - min) + min;    
-}
+        return {
+            getSway: getSway,
+            children: children,
+            first: first,
+            incrementSway: incrementSway,
+            render: render
+        };
+
+    }
+    var tree = branch(treeHeight, 0, true);
+
+
+    function animate() {
+        ctx.save();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.translate(canvas.width / 2, canvas.height - 100);
+        tree.render(ctx);
+        window.requestAnimationFrame(animate);
+        ctx.restore();
+    }
+    return {tree: tree, draw: animate};
+}(treeHeight)).draw());
